6991adfe1a6e89c3a87bea5e293d0893e7125b90
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-02-17 14:11:56 -0200

    'up'

--- www/js/index.js
+++ www/js/index.js
@@ -1,90 +1,61 @@
-document.addEventListener("deviceready", onDeviceReady, false); 
-
-
-function onDeviceReady() {
-
-//alert('teste');
 /*
-var conn = navigator.connection.type;
-if( conn == 'none' || conn == 'NONE' ){ //conn
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		app.inapp();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-alert('O aplicativo nao detectou conexao com internet.');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-} else {
-
-document.getElementById('conectando').style.display = 'none';
-//cordova.InAppBrowser.open(encodeURI('http://fazerja.com.br/site/index.php?app=s'), '_self', 'location=no,hidden=no');
-}
-
-
-document.getElementById('fechar').style.display = 'block';
-*/
-
-atualizarNew();
-
-}
-
-
-
-function fechar(){
-navigator.app.exitApp();
-}
-
-
-
-
-
-function atualizarNew(){
-
-
-var conn = navigator.connection.type;
-if( conn == 'none' || conn == 'NONE' ){ //conn
-
-alert('O aplicativo nao detectou conexao com internet.');
-
-} else {
-
-var urlBrow = 'http://fazerja.com.br/site/index.php?app=s&tapp=i';
-//alert( 'urlBrow: ' + urlBrow );
-cordova.InAppBrowser.open(encodeURI( urlBrow ), '_self', 'location=no,hidden=no');
-
-}
-
-
-
-}
+        console.log('Received Event: ' + id);
+		
+			
+    },
 	
+	inapp: function() {
 
+		//cordova.InAppBrowser.open(encodeURI('http://sisfit.net.br/mobile'), '_system', 'location=no');
 
-
-
-
-
-function atualizar(){
-
-
-var conn = navigator.connection.type;
-if( conn == 'none' || conn == 'NONE' ){ //conn
-
-alert('O aplicativo nao detectou conexao com internet.');
-
-} else {
-//cordova.InAppBrowser.open(encodeURI('http://fazerja.com.br/site/index.php?app=s'), '_self', 'location=no,hidden=no');
-
-var altTela = parseInt( screen.height ) ;
-if( altTela != '' && altTela != 'undefined'  && altTela != 0 ){
-//alert(altTela);
-document.getElementById("ifrBrowser").style.height = altTela - 70 +"px";
-}
-
-var urlBrow = 'http://fazerja.com.br/site/index.php?app=s';
-//alert( 'urlBrow: ' + urlBrow );
-ifrBrowser.location.href =  urlBrow;
-
-}
-
-
-
-}
+cordova.InAppBrowser.open(encodeURI('http://fazerja.com.br/site/index.php?app=s'), '_self', 'location=no,hidden=no');
 	   
-
+    }
+	
+};
